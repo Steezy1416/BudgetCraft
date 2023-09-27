e13c7e94bbc4acbfe2da61ac77d8262d3060728f
alexfortin <fortin.alex14@gmail.com>
2023-09-27 15:09:19 -0500

    created the working navigation menu for mobile, added an overlay for when the nav is active, and styled the nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Root from './components/Root';
+import "./style.css"
 
 
 const router = createBrowserRouter(
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Root = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <>
-      <header>
-        <button>
+      <header className="main-nav-header">
+        <div
+          className={isNavOpen && "activeOverlay"}
+          onClick={() => setIsNavOpen(!isNavOpen)}
+        ></div>
+
+        <button
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          className={`${isNavOpen ? "openNav" : "closedNav"} nav-btn`}
+        >
+          <span className="nav-btn-circle"></span>
+          <span className="nav-btn-circle"></span>
+          <span className="nav-btn-circle"></span>
           <span className="nav-btn-circle"></span>
           <span className="nav-btn-circle"></span>
           <span className="nav-btn-circle"></span>
@@ -12,11 +26,20 @@ const Root = () => {
           <span className="nav-btn-circle"></span>
           <span className="nav-btn-circle"></span>
         </button>
-        <nav>
-          <NavLink to={"/"}>Home</NavLink>
-          <NavLink to={"/expenses"}>Expenses</NavLink>
-          <NavLink to={"/transaction"}>Transaction</NavLink>
-          <NavLink to={"/history"}>History</NavLink>
+
+        <nav className={`${isNavOpen && "open-nav"} main-navigation`}>
+          <NavLink className={"navLink"} to={"/"}>
+            Home
+          </NavLink>
+          <NavLink className={"navLink"} to={"/expenses"}>
+            Expenses
+          </NavLink>
+          <NavLink className={"navLink"} to={"/transaction"}>
+            Transaction
+          </NavLink>
+          <NavLink className={"navLink"} to={"/history"}>
+            History
+          </NavLink>
         </nav>
       </header>
       <main>
